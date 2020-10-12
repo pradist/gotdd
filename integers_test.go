8f910d57dc1ef57d4c58abffd3d7644ded4fee33
Pradist Keawborisut <pradist.k@kbtg.tech>
2020-10-12 18:14:30 +0700

    Add name for test case.

--- integers_test.go
+++ integers_test.go
@@ -11,15 +11,21 @@ func TestAdder(t *testing.T) {
 		}
 	}
 
-	t.Run("", func(t *testing.T) {
+	t.Run("2 2 4", func(t *testing.T) {
 		sum := Add(2, 2)
 		expected := 4
 		assertCorrectSum(t, expected, sum)
 	})
 
-	t.Run("", func(t *testing.T) {
+	t.Run("1 2 4", func(t *testing.T) {
 		sum := Add(1, 2)
 		expected := 3
+		assertCorrectSum(t, expected, sum)
+	})
+
+	t.Run("-1 2 1", func(t *testing.T) {
+		sum := Add(-1, 2)
+		expected := 1
 		assertCorrectSum(t, expected, sum)
 	})
 }

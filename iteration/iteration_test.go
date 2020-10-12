371b237fc007360a06a8d10ed1241bd87ea382d8
Pradist Keawborisut <pradist.k@kbtg.tech>
2020-10-12 18:44:22 +0700

    Refactor repeated.

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,9 +3,47 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-	if expected != repeated {
-		t.Errorf("expected %q but got %q", expected, repeated)
+
+	assertCorrect := func(t *testing.T, expected, repeated string) {
+
+		if expected != repeated {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	}
+
+	t.Run("a to aaaaa", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrect(t, expected, repeated)
+	})
+
+	t.Run("b to bbbbb", func(t *testing.T) {
+		repeated := Repeat("b", 5)
+		expected := "bbbbb"
+		assertCorrect(t, expected, repeated)
+	})
+
+	t.Run("b to bbbbbb", func(t *testing.T) {
+		repeated := Repeat("b", 6)
+		expected := "bbbbbb"
+		assertCorrect(t, expected, repeated)
+	})
+
+	t.Run("a to aaaaaaa", func(t *testing.T) {
+		repeated := Repeat("a", 7)
+		expected := "aaaaaaa"
+		assertCorrect(t, expected, repeated)
+	})
+
+	t.Run("a to ", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+		assertCorrect(t, expected, repeated)
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", b.N)
 	}
 }

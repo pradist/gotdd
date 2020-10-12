1cfdaf72a32fecdee8f9bbd6da4c8e1f77b4f4cf
Pradist Keawborisut <pradist.k@kbtg.tech>
2020-10-12 15:40:57 +0700

    Add language to hello function.

--- hello.go
+++ hello.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if len(name) == 0 {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	prefix := englishHelloPrefix
+
+	if language == spanish {
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
